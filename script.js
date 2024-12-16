116f1809e060140305ef56f61052649895c9af5e
Floor-emi <rafaelemilianoangeleslesan@gmail.com>
2024-12-16 00:47:16 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,167 @@
+i1 = document.getElementById("box1");
+i2 = document.getElementById("box2");
+i3 = document.getElementById("box3");
+i4 = document.getElementById("box4");
+i5 = document.getElementById("box5");
+i6 = document.getElementById("box6");
+i7 = document.getElementById("box7");
+i8 = document.getElementById("box8");
+i9 = document.getElementById("box9");
+i10 = document.getElementById("box10");
+i11 = document.getElementById("box11");
+i12 = document.getElementById("box12");
+getID = document.getElementById("frontPage");
+audio1 = document.getElementById("fwtbtaudio");
+audio2 = document.getElementById("immortalaudio");
+audio3 = document.getElementById("dejaVuaudio");
+audio4 = document.getElementById("vileMentalityaudio");
+audio5 = document.getElementById("shesMinePt1audio");
+audio6 = document.getElementById("changeaudio");
+audio7 = document.getElementById("neighborsaudio");
+audio8 = document.getElementById("foldinClothesaudio");
+audio9 = document.getElementById("shesMinePt2audio");
+audio10 = document.getElementById("fyeoaudio");
+audio11 = document.getElementById("wetDreamzaudio");
+
+function forWhomTheBellTolls() {
+  document.getElementById("frontPage").innerHTML =
+    "The lyrics of the song address a variety of topics, all of which revolve around racial inequality and violence against Black people. Each verse in “For Whom the Bell Tolls” presents a different situation, but the song’s theme remains the same: there must be justice for those who have been oppressed and silenced The opening verse is a commentary on the state of the rap industry and its obsession with material possessions. J.Cole speaks about the hypocrisy of rappers who flaunt their wealth and success while ignoring the struggles of those around them. The second verse is a reflection on the everyday struggles of Black people. J.Cole speaks about how the police often target Black people and how he himself has been stopped and frisked multiple times for no reason. The third verse is a call to action. J.Cole implores his listeners to stand up for themselves and others who are oppressed. He emphasizes the need for unity and solidarity in the face of systemic racism. ";
+
+  //playsong   audio1.play();
+  pause();
+  audio1.play();
+  nopacity();
+  i1.classList.add("opacity");
+}
+
+function immortal() {
+  getID.innerText =
+    "The song begins with the lines: “Uh, I just flipped the switch (flipped, flipped) I don’t know nobody else that’s doin’ this” These lines are referring to J. Cole’s own success and the fact that he’s risen to the top of the hip-hop industry. The song then moves into a series of verses that detail the struggles that black Americans face on a daily basis, including police brutality, poverty, and a lack of resources. The chorus of the song is perhaps the most powerful aspect of the entire track. It features the lines: “How come I can’t find nothin’ that’s deep? And why do I always find somethin’ on me? What’s up with society today? Said we gotta find a better way” These lines are an indictment of the current state of American society, and the fact that many young black men feel trapped and hopeless, with no way out of their current situations.";
+  pause();
+  audio2.play();
+  //playsong
+  nopacity();
+  i2.classList.add("opacity");
+}
+function dejaVu() {
+  getID.innerText =
+    "On the surface, “Deja Vu” is a love song. It tells the story of a young man experiencing deja vu when he sees a woman he’s attracted to. The track starts with a sample of Bryson Tiller’s “Exchange,” where he sings, “This what happen when I think about you, I get in my feelings, yeah.” J. Cole takes this idea and craftily crafts a story around it. In the first verse of the song, J. Cole describes how he meets a woman at a coffee shop, and they hit it off immediately. They exchanged numbers, and he’s eager to get to know her better. The chorus goes: “Seen you before, I think that I know her Shawty a dime, yeah, maybe a four-point Number so high, but she a low-key Runnin’ from love, Shawty been feenin’ for dope” These lyrics paint a picture of a woman who’s beautiful, but also guarded, and hard to read. The chorus suggests that J. Cole is certain he’s met her before (similar to the concept of deja vu), but he can’t put his finger on where or when. ";
+  pause();
+  audio3.play();
+  //playsong
+  nopacity();
+  i3.classList.add("opacity");
+}
+function vileMentality() {
+  getID.innerText =
+    "The lyrics of “Ville Mentality” reflect on the challenges of living in poverty and the impact it has on individuals, families, and communities. Cole describes how growing up in a poor neighborhood can affect one’s mindset and create a vicious cycle of hopelessness and despair. The phrase “Ville Mentality” is a play on words, referring both to his hometown and the mindset that comes with living in poverty.The song opens with the lines, “East side Johnny big redemption/No co-sign needed, I’m just shining.” Here, Cole is referencing his ability to make a name for himself despite not having the support of a major label backing him up. He goes on to describe how he was able to overcome the odds and achieve success. “B*tch I’m from Fayetteville, b*tch I’m from the ville/Brick city, country n*gga, never ever will I switch.” Throughout the song, Cole reflects on the importance of self-reflection and taking ownership of one’s life. He urges listeners to take control of their destiny and not fall victim to circumstances. “Either way you choose, life gon’ hit you/You gotta take a stand, that’s how they did you/Made a mountain of pride, that’s how they see you.” ";
+  //playsong
+  pause();
+  audio4.play();
+  nopacity();
+  i4.classList.add("opacity");
+}
+function shesMinePt1() {
+  getID.innerText =
+    "The first verse of the song starts with, “Catch me, don’t you…” which J. Cole uses as a metaphor for his feelings of anxiety while embracing his new role as a father. He is worried that he will drop his bundle, and his daughter will be hurt. He then goes on to express his desire to provide his daughter with the upbringing he never had as a child. The chorus of the song is an ode to Cole’s wife, expressing his deep appreciation and love for her. He credits her for helping him “seal the deal” to become the man he is today, and she also features prominently in the second verse of the song. Here J. Cole opens up about the miscarriage his wife experienced before the birth of their daughter. The lyrics detail his own emotional turmoil, dealing with the pain of that experience, and the fact that two innocent lives were lost. The song ends with a reference to Tupac Shakur’s song “Brenda’s Got a Baby,” a song that inspired J. Cole to produce “She’s Mine Pt.1.” The lyrics evoke the memory of the struggles he witnessed growing up and the harsh realities of the world. This is a poignant reminder that despite the joy of becoming a parent, the world is still a hard place, and there are harsh truths Cole must teach to his daughter.";
+  //playsong
+  pause();
+  audio5.play();
+  nopacity();
+  i5.classList.add("opacity");
+}
+function change() {
+  getID.innerText =
+    "“Change” is a powerful critique of racism and police brutality. J. cole describes the fear that black people face every day, “Look at what they did to my sister last summer, for the same crime as a f*****’ wonder.” The injustices faced by black people in America are not new, but they are often ignored. J. Cole challenges the listener to confront this reality and take action. The following lines speak to the lack of accountability and justice, “If it’s smoke, it’s gon’ rise, and it’s gon’ burn  Just like this blunt I’m ashing, time to cash in  They gave us guns and some dope, called us thugs  And left us to rot, feeling like us.” J. Cole comments on the systemic issue of vilifying black people and always managing to find a way to blame them. it’s not a cry for help; rather, it is a call to action. J. Cole offers the listener hope and empowerment in the face of adversity. In the chorus, he sings, “It’s time to change the story, it’s time to change the story.” The repetition of this phrase shows that now is the time for change. The final verse speaks to the power of unity and empathy, “Empathy and unity, that sh** is all we need  Besides power for us to evenly distribute Look at all the stores you looted, it’s proof of the truth” ";
+  //playsong
+  pause();
+  audio6.play();
+  nopacity();
+  i6.classlist.add("opacity");
+}
+function neighbors() {
+  getID.innerText =
+    "In the opening line, J. Cole raps “I guess the neighbors think I’m sellin’ dope,” suggesting that he is being racially profiled by the people in his neighborhood. This line sets the tone for the rest of the song, highlighting the constant suspicion and lack of trust that black people face in predominantly white neighborhoods. The second verse talks about how the police’s aggression towards black people is amplified in white neighborhoods. J. Cole raps, “Cops wanna search the vehicle, I ain’t consentin’ / Unless they see a warrant, they can go to hell, I’m not resistin’.” He is protesting against the systemic racism in law enforcement, where black people are seen as criminals first and are stopped and searched without reasonable cause. In the last few lines of the song, J. Cole ends with a poignant message, “Time ticking, everybody’s a victim / Blood dripping, everybody’s a chicken.” This line reveals the helplessness and hopelessness that he and many other black people feel when faced with systemic racism. No one is exempt, and everyone is affected by the injustice that permeates society.";
+  //playsong
+  pause();
+  audio7.play();
+  nopacity();
+  i7.classList.add("opacity");
+}
+function foldinClothes() {
+  getID.innerText =
+    "J. Cole’s “Foldin Clothes” is a lyrical masterpiece. The song has a soulful beat that complements the lyrics perfectly. It is essentially a love song that captures all the tiny details of a perfect day spent with a partner. “Foldin Clothes” is a love song that celebrates togetherness and the little moments of happiness that make a relationship special. The song talks about the joys of sharing domestic life with your partner and how doing mundane chores like folding clothes can bring you closer to each other.";
+  //playsong
+  pause();
+  audio8.play();
+  nopacity();
+  i8.classList.add("opacity");
+}
+function shesMinePt2() {
+  getID.innerText =
+    "From the first lines of “She’s Mine Pt. 2,” it’s clear that the song is a love letter to Cole’s daughter. He raps: “My intuition is telling me there’ll be better days I sit in silence and find whenever I meditate My fears alleviate, my tears evaporate My faith don’t deviate, ideas don’t have a date But see I’m growing and getting stronger with every breath Bringing me closer to Heaven’s doors with every step As we speak I’m in peace, no longer scared to die Most n****s don’t believe in God and so they terrified” These words paint a vivid picture of the profound impact fatherhood has had on Cole’s life. He finds comfort in his daughter, and sees her as a source of hope and inspiration. The lines also reveal the anxiety and fear that can often come with parenthood, as well as the strength and resilience that fatherhood can cultivate. Throughout the song, Cole reflects on his own upbringing and the lessons he learned from his parents. He acknowledges the sacrifices his mother made, and the fear he felt when his father went to prison. He raps: “To all the daughters of the world, never settle For polished diamonds or just shiny metal Cause n****s will get you in trouble, girlfriend Learn from me, don’t you ever ever ever ever chase a man” These lines speak to the importance of self-worth and self-respect, and the lessons Cole hopes to pass down to his own daughter. He wants her to have the confidence and resilience to navigate the world on her own terms, and to never settle for anything less than what she deserves.";
+  //playsong
+  pause();
+  audio9.play();
+  nopacity();
+  i9.classList.add("opacity");
+}
+function forYourEyezOnly() {
+  getID.innerText =
+    "The song opens with a sample from Nina Simone’s “Baltimore,” which sets the tone for the somber reflection that is to follow. J.Cole raps, “I hope you hear this message and not scrutinize my method, to my unborn seed, hope you know that you’re special.” The lyrics reveal J.Cole’s desire to impart a message to his child, warning of the dangers of the world and the prevalence of violence. He speaks of his friend James, who was caught up in the violent world of drug dealing, and who ultimately lost his life as a result. J.Cole also addresses the impact that violence has on the families left behind, rapping, “And if I die before your album drop, I hope-” before trailing off. The lyric is an allusion to the fact that James’ mother reached out to J.Cole after her son’s death, asking him to tell James’ story through his music.";
+  //playsong
+  pause();
+  audio10.play();
+  nopacity();
+  i10.classList.add("opacity");
+}
+function reset() {
+  pause();
+  audio10.currentTime = 0;
+  audio1.currentTime = 0;
+  audio2.currentTime = 0;
+  audio3.currentTime = 0;
+  audio4.currentTime = 0;
+  audio5.currentTime = 0;
+  audio6.currentTime = 0;
+  audio7.currentTime = 0;
+  audio8.currentTime = 0;
+  audio9.currentTime = 0;
+  audio11.currentTime = 0;
+  getID.innerText =
+    "click the different images to play the songs from the tracklist to your left";
+  i12.src = "images/middle/kid12.png";
+  nopacity();
+}
+function favSong() {
+  pause();
+  audio11.play();
+  getID.innerText =
+    "congrats! you've found my favorite song (even though it's not on the album)";
+  i12.src = "images/middle/me.png";
+}
+function pause() {
+  audio10.pause();
+  audio1.pause();
+  audio2.pause();
+  audio3.pause();
+  audio4.pause();
+  audio5.pause();
+  audio6.pause();
+  audio7.pause();
+  audio8.pause();
+  audio9.pause();
+  audio11.pause();
+}
+function nopacity() {
+  i1.classList.remove("opacity");
+  i2.classList.remove("opacity");
+  i3.classList.remove("opacity");
+  i4.classList.remove("opacity");
+  i5.classList.remove("opacity");
+  i6.classlist.remove("opacity");
+  i7.classList.remove("opacity");
+  i8.classList.remove("opacity");
+  i9.classList.remove("opacity");
+  i10.classList.remove("opacity");
+}
